8701a00229198390e2eceeb2da22a83f387a69ba
Obi <obi.netbits@outlook.com>
2016-06-21 22:47:06 +0000

    add sign up form, add sign up routes

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
     
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
+
+
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "One day I will be the best nodejs programmer humanly possible",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -12,7 +31,27 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret");
-})
+});
+
+//Auth Routes
+
+//SHOW SIGN UP FORM
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//HANDLING USER SIGN UP
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started............");
