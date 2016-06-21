135ade864f31890b1f7d64e7a23b743438c87799
Obi <obi.netbits@outlook.com>
2016-06-21 15:52:34 +0000

    initial push

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    mongoose = require("mongoose");
+    
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret", function(req, res){
+    res.render("secret");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server started............");
+});
